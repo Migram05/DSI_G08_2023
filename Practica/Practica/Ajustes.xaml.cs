3f10d4789ae40634833c94147f46bd90f713610c
Migram05 <100275263+Migram05@users.noreply.github.com>
2023-04-05 13:52:11 +0200

    Pantalla de ajustes terminada
    La pantalla de ajustes tiene todos los elementos añadidos con sus funcionalidades

--- Practica/Practica/Ajustes.xaml.cs
+++ Practica/Practica/Ajustes.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Globalization;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,5 +27,26 @@ namespace Practica
         {
             this.InitializeComponent();
         }
+
+        //Navegación a páginas
+        private void ImagenMenuPrincipal(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void BotonAjustesSociales_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AjustesSociales));
+        }
+        private void languageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Obtener el idioma seleccionado del ComboBoxItem seleccionado
+            ComboBoxItem selectedItem = languageComboBox.SelectedItem as ComboBoxItem;
+            string selectedLanguage = selectedItem.Tag.ToString();
+
+            // Cambiar el idioma de la aplicación a través del objeto ApplicationLanguages
+            ApplicationLanguages.PrimaryLanguageOverride = selectedLanguage;
+        }
+
     }
 }
